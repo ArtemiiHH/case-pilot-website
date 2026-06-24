424bce452432bebc0f1caf441987932a0f7f7e76
ArtemiiHH <artemiishchukin2003@gmail.com>
2026-06-24 20:33:06 +0300

    Try another graphic

--- src/components/ProductGraphic/ProductGraphic.jsx
+++ src/components/ProductGraphic/ProductGraphic.jsx
@@ -1,12 +1,13 @@
 import styles from "./ProductGraphic.module.css";
 import dashboardGraphic from "../../images/dashboard-graphic-noshadow.png";
+import dashboardGraphicTwo from "../../images/dashboard-graphic2.png";
 
 export default function ProductGraphic() {
   return (
     <div className={styles.container}>
       <img
-        className={styles.graphic}
-        src={dashboardGraphic}
+        className={styles.graphicTwo}
+        src={dashboardGraphicTwo}
         alt="CasePilot dashboard"
       />
     </div>
