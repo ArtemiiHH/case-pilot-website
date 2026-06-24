fc2d89c073c8b4d4e709a5e8c14728e48ac566db
ArtemiiHH <artemiishchukin2003@gmail.com>
2026-06-24 21:31:24 +0300

    Remove second dashboard graphic

--- src/components/ProductGraphic/ProductGraphic.jsx
+++ src/components/ProductGraphic/ProductGraphic.jsx
@@ -1,13 +1,12 @@
 import styles from "./ProductGraphic.module.css";
 import dashboardGraphic from "../../images/dashboard-graphic-noshadow.png";
-import dashboardGraphicTwo from "../../images/dashboard-graphic2.png";
 
 export default function ProductGraphic() {
   return (
     <div className={styles.container}>
       <img
-        className={styles.graphicTwo}
-        src={dashboardGraphicTwo}
+        className={styles.graphic}
+        src={dashboardGraphic}
         alt="CasePilot dashboard"
       />
     </div>
